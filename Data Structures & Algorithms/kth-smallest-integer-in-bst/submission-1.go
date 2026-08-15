88a9cb52f7484c30c268c864900a4526b8bc7557
Luis David Fuentes Juvera <34464283+LuisDFJ@users.noreply.github.com>
2026-08-15 11:48:18 -0600

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func kthSmallest(root *TreeNode, k int) int {
+    var res int
+    var dfs func(*TreeNode)
+    dfs = func(root *TreeNode) {
+        if root == nil { return }
+        dfs(root.Left)
+        k -= 1
+        if k == 0 {res = root.Val}
+        dfs(root.Right)
+    }
+    dfs(root)
+    return res
+}
